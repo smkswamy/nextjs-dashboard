44cda8c2b51877bc85531f7ccfcd25e53e0ca2fa
Manikantha Sunkavalli <smkswamy123@gmail.com>
2025-01-20 12:28:36 -0500

    Invoice page changes

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -101,7 +101,11 @@
 //   return insertedRevenue;
 // }
 
-// export async function GET() {
+export async function GET() {
+    return Response.json({
+        message:
+          'Uncomment this file and remove this line. You can delete this file when you are finished.',
+      });
 //   try {
 //     await client.sql`BEGIN`;
 //     await seedUsers();
@@ -115,4 +119,4 @@
 //     await client.sql`ROLLBACK`;
 //     return Response.json({ error }, { status: 500 });
 //   }
-// }
+}
