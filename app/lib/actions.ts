30b34b6939dce6093c8276148c6dc66bfe3a6828
Manikantha Sunkavalli <smkswamy123@gmail.com>
2025-01-21 09:38:41 -0500

    Fixed linting issues

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -61,6 +61,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     `;
   } catch (error) {
     // If a database error occurs, return a more specific error.
+    console.log(error);
     return {
       message: 'Database Error: Failed to Create Invoice.',
     };
@@ -101,6 +102,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
   } catch (error) {
+    console.log(error);
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
  
